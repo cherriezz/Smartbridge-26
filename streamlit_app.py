bf058661353038105707d588397fa3f54cdbb809
cherriezz <mylittlespace15@gmail.com>
2026-02-12 20:09:14 +0530

    Updated UI, added About section, improved layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,11 @@ import requests
 
 st.set_page_config(layout="wide")
 
-# Run Flask app
-if "server_started" not in st.session_state:
+if "started" not in st.session_state:
 
     subprocess.Popen(["python", "flask/app.py"])
+    st.session_state.started = True
 
-    st.session_state.server_started = True
-
-    # wait for flask to start
     for _ in range(10):
         try:
             requests.get("http://127.0.0.1:5000")
@@ -20,7 +17,6 @@ if "server_started" not in st.session_state:
         except:
             time.sleep(1)
 
-# Embed Flask site
 st.components.v1.iframe(
     "http://127.0.0.1:5000",
     height=900,
